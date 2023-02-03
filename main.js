5cf852b29c3f5d3d5b49725f3e473d03c6fdff30
nn-3 <57413141+nn-3@users.noreply.github.com>
2023-02-03 14:10:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ let set = Object.assign({
 },opt||{});
 let keys = Object.keys(dflt);
 let data = Object.create(dflt);
-let storage, sFlg = set.storage!==null;
-if(sFlg) {
+let storage, sF = set.storage!==null;
+if(sF) {
 	storage = window[set.storage+'Storage']||window.sessionStorage;
 	data = Object.assign(data,JSON.parse(storage.getItem(set.path))||{});
 }
@@ -26,73 +26,75 @@ function setting(){
 	let $clr = $id(set.clrBtnId);
 	let $list = {};
 	let bET = typeof set.btnEvent;
-	let lFlg = (!!$set||!!clr) && (bET=='function'||!sFlg);
+	let bF = !!$set||!!$clr;
+	let iF = false;
+	let lF = bF && (bET=='function'||!sF);
+	let nF = typeof textChangerNodes=='object' && Array.isArray(textChangerNodes);
 	for(let k of keys){
 		$list[k] = {};
-		if(lFlg) $list[k].node = [];
+		if(lF) $list[k].node = [];
 	}
 	$d.querySelectorAll('['+set.attr+']').forEach($e=>{
 		let k = $e.getAttribute(set.attr);
 		if(keys.indexOf(k)<0) return;
 		if($e.tagName=='INPUT'){
+			if(!bF) return;
 			$e.value = data[k];
 			$list[k].input = $e;
+			iF = true;
 		} else {
 			$e.textContent = data[k];
-			if(lFlg) $list[k].node.push($e.childNodes[0]);
+			if(lF) $list[k].node.push($e.childNodes[0]);
+			if(nF) textChangerNodes.push($e.childNodes[0]);
 		}
 	});
-	if(set.word && /key|default/.test(set.word)) {
-		let regEsc = str => str.replace(/[\-\/\\|^$*+?.(){}\[\]]/g,'\x5c$&');
-		let w={},esc=[],r;
-		if(0<=set.word.indexOf('key')){
-			for(let k of keys) {
-				w[k] = k;
-				esc.push(regEsc(k));
-			}
-			r = /key/;
-		} else {
-			for(let k of keys) {
-				w[dflt[k]] = k;
-				esc.push(regEsc(dflt[k]));
-			}
-			r = /default/;
-		}
-		let reg = RegExp(regEsc(set.word).replace(r,'('+esc.join('|')+')'),lFlg?'':'g');
-		let rTN, rTNs = $elm =>{
-			let ns = Array.from($elm.childNodes);
-			for(let n of ns){
-				if(n.nodeType==1&&!n.tagName!='script') rTNs(n);
-				else if(n.nodeType==3) rTN(n);
-			}
-		}
-		if(lFlg) {
-			rTN = n =>{
-				let m = n.nodeValue.match(reg);
-				if(!!m){
-					let k = w[m[1]];
-					let l = m.index+m[0].length;
-					if(m.input.length!=l) rTN(n.splitText(l));
-					let nn = !m.index?n:n.splitText(m.index);
-					$list[k].node.push(nn);
-					nn.nodeValue = data[k];
+	if(set.word) {
+		let t = set.word.match(/key|default/);
+		if(t){
+			let regEsc = str => str.replace(/[\-\/\\|^$*+?.(){}\[\]]/g,'\x5c$&');
+			let wP = (e,k) => {w[e]=k;reg+=(reg.length?'|':'')+regEsc(e)};
+			let w={},reg='';
+			let rTN, rTNs = $elm =>{
+				let ns = Array.from($elm.childNodes);
+				for(let n of ns){
+					if(n.nodeType==1&&!n.matches('script,['+set.attr+']')) rTNs(n);
+					else if(n.nodeType==3) rTN(n);
 				}
 			}
-		} else {
-			let rep = (s,k) => data[w[k]]||s;
-			rTN = n => n.nodeValue = n.nodeValue.replace(reg,rep);
+			if(t[0]=='key') for(let k of keys) wP(k,k);
+			else for(let k of keys) wP(dflt[k],k);
+			reg = regEsc(set.word).replace(RegExp(t[0]),'('+reg+')');
+			if(lF||nF) {
+				reg = RegExp(reg);
+				rTN = n =>{
+					if(nF&&textChangerNodes.includes(n)) return;
+					let m = n.nodeValue.match(reg);
+					if(!!m){
+						let l = m.index+m[0].length;
+						if(m.input.length!=l) rTN(n.splitText(l));
+						let nn = !m.index?n:n.splitText(m.index);
+						if(lF) $list[w[m[1]]].node.push(nn);
+						if(nF) textChangerNodes.push(nn);
+						nn.nodeValue = data[w[m[1]]];
+					}
+				}
+			} else {
+				reg = RegExp(reg,'g');
+				let rep = (s,k) => data[w[k]]||s;
+				rTN = n => n.nodeValue = n.nodeValue.replace(reg,rep);
+			}
+			rTNs($area);
 		}
-		rTNs($area);
 	}
-	if(!!set||!!clr){
+	if(iF&&bF){
 		let iEach = fnc =>{
 			for(let k of keys) if(!!$list[k].input) fnc(k,$list[k].input);
 		}
-		let sSet = sFlg?()=>{
+		let sSet = sF?()=>{
 			if(Object.keys(data).length) storage.setItem(set.path,JSON.stringify(data));
 			else storage.removeItem(set.path);
 		}:()=>{};
-		let bEv = lFlg?(type,obj)=>{
+		let bEv = lF?(type,obj)=>{
 			iEach((k,$i)=>{
 				$i.value = obj[k];
 				$list[k].node.forEach(n=>n.nodeValue=obj[k]);
@@ -101,7 +103,7 @@ function setting(){
 		}:bET=='string'?()=>{location.href=set.btnEvent}:()=>{location.reload()};
 		if(!!$set) {
 			$set[on]('click',()=>{
-				iEach((k,$i)=>data[k]=$i.value)
+				iEach((k,$i)=>data[k]=$i.value);
 				sSet();
 				bEv('set',data);
 			});
@@ -115,11 +117,7 @@ function setting(){
 		}
 	}
 }
-
-if($d.readyState=='loading') {
-	$d[on]('DOMContentLoaded',setting);
-} else {
-	setting();
-}
+if($d.readyState=='loading') $d[on]('DOMContentLoaded',setting);
+else setting();
 
 }
