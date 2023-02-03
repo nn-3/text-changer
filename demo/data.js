9157cec793ef9b440b78f96545dcb2ab81a178d5
nn-3 <57413141+nn-3@users.noreply.github.com>
2023-02-03 14:24:31 +0900

    Update data.js

--- demo/data.js
+++ demo/data.js
@@ -1,16 +1,18 @@
-textChanger({
-		'姓':'見本',
-		'名':'例子'
-	},{
-		btnEvent:t=>{
-			let $pop = document.createElement('div');
-			let flg = false;
-			$pop.classList.add('popup');
-			$pop.textContent = `設定を${t=='set'?'変更':'初期化'}しました`;
-			document.body.appendChild($pop);
-			$pop.addEventListener('animationend',e=>{
-				if(!flg) flg = true;
-				else document.body.removeChild($pop);
-			});
-		}
+
+document.addEventListener('animationend',e=>{
+	let $pop = e.target;
+	if(!$pop.classList.contains('popup')) return;
+	if($pop.dataset.visible=='1') document.body.removeChild($pop);
+	else $pop.dataset.visible = '1';
 });
+let pop = t =>{
+	let $pop = document.createElement('div');
+	$pop.classList.add('popup');
+	$pop.textContent = `設定を${t=='set'?'変更':'初期化'}しました`;
+	document.body.appendChild($pop);
+}
+
+textChanger({
+	'姓':　'見本',
+	'名':　'例子'
+},{btnEvent:　pop});
